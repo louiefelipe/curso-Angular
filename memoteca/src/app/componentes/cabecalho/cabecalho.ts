d0610dde55f7ca58e8b338d58ac9c54c5b50bd27
louiefelipe <louiefelipe288@gmail.com>
2025-12-29 21:21:58 -0300

    cabecalho

---
+++ memoteca/src/app/componentes/cabecalho/cabecalho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecalho',
+  standalone: true,
+  imports: [ ],
+  templateUrl: './cabecalho.html',
+  styleUrl: './cabecalho.css',
+})
+export class CabecalhoComponent {
+
+}
