ef8578d45ef141b9cb620190dd972404494f6787
louiefelipe <louiefelipe288@gmail.com>
2026-01-06 01:05:02 -0300

    forms

--- memoteca/src/app/app.ts
+++ memoteca/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho';
+import { Rodape } from "./componentes/rodape/rodape";
+import { CriarPensamento } from "./componentes/pensamentos/criar-pensamento/criar-pensamento";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent],
+  imports: [RouterOutlet, CabecalhoComponent, Rodape, CriarPensamento],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
