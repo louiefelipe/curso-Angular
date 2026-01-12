72b9e37bddaea2fc5ae7055a1dcc9bb7717a77c0
louiefelipe <louiefelipe288@gmail.com>
2026-01-11 22:46:42 -0300

    aaaaaaa

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { Pensamento } from "../pensamento/pensamento";
+import { NgForOf } from '@angular/common';
+
 
 @Component({
   selector: 'app-listar-pensamento',
-  imports: [RouterLink, Pensamento],
+  imports: [RouterLink, Pensamento, NgForOf],
   templateUrl: './listar-pensamento.html',
   styleUrl: './listar-pensamento.css',
 })
 export class ListarPensamento {
+  listaPensamentos = [
+    {
+    conteudo: 'Angular teste',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+  ];
 
 }
